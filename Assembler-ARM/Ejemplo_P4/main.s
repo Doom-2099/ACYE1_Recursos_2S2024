f44ca69eb79794bffab2270cc50dacecf162a8f2
Jorge Castañeda <jorge.c@lynxleads.com>
2024-10-03 01:13:25 -0600

    [add] Calculo tiempo de ejecucion y escritura de archivos

--- Assembler-ARM/Ejemplo_P4/main.s
+++ Assembler-ARM/Ejemplo_P4/main.s
@@ -1,4 +1,6 @@
+.global getFilename
 .global openFile
+.global openReport
 .global closeFile
 .global readCSV
 .global atoi
@@ -37,6 +39,21 @@ readSuccess:
     .asciz "El Archivo Se Ha Leido Correctamente\n"
     lenReadSuccess = .- readSuccess
 
+createSucces:
+    .asciz "El Reporte Se Ha Abierto Correctamente\n"
+    lenCreateSuccess = .- createSucces
+
+msgExecuteTime:
+    .asciz "El tiempo de ejecucion fue de: "
+    lenExecute = .- msgExecuteTime
+
+prefixSec:
+    .asciz " segundos "
+    lenPrefixSec = .- prefixSec
+
+prefixMicro:
+    .asciz " microsegundos\n"
+    lenPrefixMicro = .- prefixMicro
 
 .bss
 opcion:
@@ -60,11 +77,17 @@ character:
 fileDescriptor:
     .space 8
 
-.text
+timeStart:
+    .xword 0, 0
+    
+timeEnd:
+    .xword 0, 0
 
+
+.text
 // Macro para imprimir strings
-.macro print reg, len
-    MOV x0, 1
+.macro print stdout, reg, len
+    MOV x0, \stdout
     LDR x1, =\reg
     MOV x2, \len
     MOV x8, 64
@@ -80,6 +103,31 @@ fileDescriptor:
     SVC 0
 .endm
 
+.macro getTime storage
+    LDR x0, =\storage
+    MOV x1, 0
+    MOV x8, 169
+    SVC 0
+.endm
+
+getFilename:
+    print 1, msgFilename, lenMsgFilename
+    read 0, filename, 50
+
+    // Agregar caracter nulo al final del nombre del archivo
+    LDR x0, =filename
+    loop:
+        LDRB w1, [x0], 1
+        CMP w1, 10
+        BEQ endLoop
+        B loop
+
+        endLoop:
+            MOV w1, 0
+            STRB w1, [x0, -1]!
+    
+    RET
+
 openFile:
     // param: x1 -> filename
     MOV x0, -100
@@ -94,12 +142,35 @@ openFile:
     B op_f_end
 
     op_f_error:
-        print errorOpenFile, lenErrOpenFile
+        print 1, errorOpenFile, lenErrOpenFile
         read 0, opcion, 1
 
     op_f_end:
         RET
 
+openReport:
+    MOV x0, -100        // open
+    LDR x1, =filename   // filename address
+    MOV x2, 101         // O_WRONLY | O_CREAT
+    MOV x3, 0777        // permissions
+    MOV x8, 56          // openat
+    SVC #0              // syscall
+
+    CMP x0, 0
+    BLE op_r_error
+    LDR x9, =fileDescriptor
+    STR x0, [x9]
+    B op_r_end
+
+    op_r_error:
+        print 1, errorOpenFile, lenErrOpenFile
+        read 0, opcion, 1
+        RET
+
+    op_r_end:
+        print 1, createSucces, lenCreateSuccess
+        read 0, opcion, 1
+        RET
 
 closeFile:
     LDR x0, =fileDescriptor
@@ -151,8 +222,8 @@ readCSV:
             CBNZ x20, rd_num
 
     rd_end:
-        print salto, lenSalto
-        print readSuccess, lenReadSuccess
+        print 1, salto, lenSalto
+        print 1, readSuccess, lenReadSuccess
         read 0, opcion, 2
         RET
 
@@ -208,12 +279,22 @@ itoa:
     // params: x0 => number, x1 => buffer address
     MOV x10, 0  // contador de digitos a imprimir
     MOV x12, 0  // flag para indicar si hay signo menos
-    MOV w2, 10000  // Base 10
-    CMP w0, 0  // Numero a convertir
-    BGT i_convertirAscii
-    CBZ w0, i_zero
 
-    B i_negative
+    CBZ x0, i_zero
+
+    MOV x2, 1
+    defineBase:
+        CMP x2, x0
+        MOV x5, 0
+        BGT cont
+        MOV x5, 10
+        MUL x2, x2, x5
+        B defineBase
+
+    cont:
+        CMP x0, 0  // Numero a convertir
+        BGT i_convertirAscii
+        B i_negative
 
     i_zero:
         ADD x10, x10, 1
@@ -225,33 +306,33 @@ itoa:
         MOV  x12, 1
         MOV w5, 45
         STRB w5, [x1], 1
-        NEG w0, w0
+        NEG x0, x0
 
     i_convertirAscii:
-        CBZ w2, i_endConversion
-        UDIV w3, w0, w2
-        CBZ w3, i_reduceBase
+        CBZ x2, i_endConversion
+        UDIV x3, x0, x2
+        CBZ x3, i_reduceBase
 
         MOV w5, w3
         ADD w5, w5, 48
         STRB w5, [x1], 1
         ADD x10, x10, 1
 
-        MUL w3, w3, w2
-        SUB w0, w0, w3
+        MUL x3, x3, x2
+        SUB x0, x0, x3
 
-        CMP w2, 1
+        CMP x2, 1
         BLE i_endConversion
 
         i_reduceBase:
-            MOV w6, 10
-            UDIV w2, w2, w6
+            MOV x6, 10
+            UDIV x2, x2, x6
 
-            CBNZ w10, i_addZero
+            CBNZ x10, i_addZero
             B i_convertirAscii
 
         i_addZero:
-            CBNZ w3, i_convertirAscii
+            CBNZ x3, i_convertirAscii
             ADD x10, x10, 1
             MOV w5, 48
             STRB w5, [x1], 1
@@ -259,7 +340,6 @@ itoa:
 
     i_endConversion:
         ADD x10, x10, x12
-        print num, x10
         RET
 
 
@@ -268,7 +348,7 @@ bubbleSort:
     LDR x0, [x0] // length => cantidad de numeros leidos del csv
 
     MOV x1, 0 // index i - bubble sort algorithm
-    SUB x0, x0, 1 // length - 1
+    SUB x0, x0, 1 // length - 1 => x0
 
     bs_loop1:
         MOV x9, 0 // index j - bubble sort algorithm
@@ -301,27 +381,14 @@ bubbleSort:
 // Etiqueta de inicio del programa
 _start:
     // Limpiar salida de la terminal
-    print clear_screen, lenClear
+    print 1, clear_screen, lenClear
 
     // Imprimir encabezado
-    print encabezado, lenEncabezado
+    print 1, encabezado, lenEncabezado
     read 0, opcion, 2
 
     // Imprimir mensaje para ingresar el nombre del archivo
-    print msgFilename, lenMsgFilename
-    read 0, filename, 50
-
-    // Agregar caracter nulo al final del nombre del archivo
-    LDR x0, =filename
-    loop:
-        LDRB w1, [x0], 1
-        CMP w1, 10
-        BEQ endLoop
-        B loop
-
-        endLoop:
-            MOV w1, 0
-            STRB w1, [x0, -1]!
+    BL getFilename
 
     // funcion para abrir el archivo
     LDR x1, =filename
@@ -334,7 +401,11 @@ _start:
     BL closeFile 
 
     // Llamar Algoritmo de Ordenamiento Burbuja
+    getTime timeStart
+
     BL bubbleSort
+
+    getTime timeEnd
 
     // recorrer array y convertir a ascii
     LDR x9, =count
@@ -346,14 +417,52 @@ _start:
         LDRH w0, [x15], 2
         LDR x1, =num
         BL itoa
-
-        print espacio, lenEspacio
+        
+        print 1, num, x10
+        print 1, espacio, lenEspacio
 
         ADD x7, x7, 1
         CMP x9, x7
         BNE loop_array
 
-    print salto, lenSalto
+    print 1, salto, lenSalto 
+    
+    // Escribir tiempo de ejecucion en archivo de texto
+    BL getFilename
+    BL openReport
+
+    LDR x20, fileDescriptor
+    print x20, msgExecuteTime, lenExecute
+
+    LDR x0, =timeStart
+    LDR x1, =timeEnd
+    LDR x2, [x0]
+    LDR x3, [x1]
+
+    LDR x4, [x0, 8]
+    LDR x5, [x1, 8]
+
+    SUB x3, x3, x2 
+    SUBS x5, x5, x4
+    CNEG x3, x3, MI
+
+    MOV x15, x5
+
+    MOV x0, x3
+    LDR x1, =num
+    BL itoa
+
+    print x20, num, x10
+    print x20, prefixSec, lenPrefixSec
+
+    MOV x0, x15
+    LDR x1, =num
+    BL itoa
+
+    print x20, num, x10
+    print x20, prefixMicro, lenPrefixMicro
+
+    BL closeFile
 
     // Instruccion para terminar el programa
     end:
